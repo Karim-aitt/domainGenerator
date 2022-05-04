cce577c4ff5756dfa185fa4a97c6519a3f1f2054
Karim <karim.gonzalez.ait@gmail.com>
2022-05-04 17:34:11 +0000

    update

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ let result = "";
 for(let i in pronoun){
     for(let j in adj){
         for(let k in noun){
-            result += pronoun[j]+adj[i]+noun[k]+".com"+`\n`
+            result += pronoun[j]+adj[i]+noun[k]+".com"+`\n`+"<br>"
         }
     }
 }
 
 console.log(result);
 
-// Paso 4 del ejercicio:  run el código con: "$ node app.js"   en la terminal  
+// Paso 4 del ejercicio:  run el código con: "$ node app.js"   en la terminal 
+
+document.getElementById("domain").innerHTML = `${result}`
